e1baa4cfcd93949d278ff54d0a86bb833efe1631
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-04 18:25:28 -0500

    finshed lab

--- src/main/java/com/zipcodewilmington/streams/StreamMap.java
+++ src/main/java/com/zipcodewilmington/streams/StreamMap.java
@@ -16,24 +16,26 @@ public class StreamMap {
      * Section 8.3
      * @param someWord - word to convert to Stream<String>
      * @return - a Stream of single characters
-     */ //TODO
+     */
     public static Stream<String> letters(String someWord) {
-        return null;
+        return Stream.of(someWord);
     }
 
     /**
      * @param someWords - variable amount of String arguments
      * @return - a Stream of several Streams of single characters
-     */ //TODO
+     */
     public static Stream<Stream<String>> wordsMap(String... someWords) {
-        return null;
+        return Stream.of(someWords).map(StreamMap::letters);
+        //return Stream.of(someWords).map(word -> letters(word));
     }
 
     /**
      * @param stringArray - variable amount of String arguments
      * @return - a Stream of several Streams of single characters
-     */ //TODO
+     */
     public static Stream<String> wordsFlatMap(String... stringArray) {
-        return null;
+        return Stream.of(stringArray).flatMap(StreamMap::letters);
+        //return Stream.of(stringArray).flatMap(word -> letters(word));
     }
 }
